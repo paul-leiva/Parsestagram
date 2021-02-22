05daf92c8972a87e14d8b2a9301b043e1f09c980
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2021-02-22 03:34:21 -0500

    added posting and logout

--- app/src/main/java/com/example/loginactivity/ParseApplication.java
+++ app/src/main/java/com/example/loginactivity/ParseApplication.java
@@ -3,11 +3,15 @@ package com.example.loginactivity;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("lEtkipxGG7J6q53Qx6bproniiNdBdYz4MMHKSpsv")
